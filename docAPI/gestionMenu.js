6a333fdc69f1e2a1105f0ec0958a0242398b054b
Christophe Ferru <christophe.ferru@gmail.com>
2021-08-30 17:02:05 -0400

    fin des documentations de compte

--- docAPI/gestionMenu.js
+++ docAPI/gestionMenu.js
@@ -3,17 +3,26 @@ const gererPanneau = (idMenu) => {
         case 'accueil':
             document.getElementById('accueilAPI').classList.remove("d-none");
             document.getElementById('creerCompte').classList.add("d-none");
+            document.getElementById('verifierCompte').classList.add("d-none");
             document.getElementById('connexion').classList.add("d-none");
             break;
         case 'creerCompte':
             document.getElementById('accueilAPI').classList.add("d-none");
             document.getElementById('connexion').classList.add("d-none");
+            document.getElementById('verifierCompte').classList.add("d-none");
             document.getElementById('creerCompte').classList.remove("d-none");
             break;
         case 'connexion':
             document.getElementById('accueilAPI').classList.add("d-none");
             document.getElementById('creerCompte').classList.add("d-none");
+            document.getElementById('verifierCompte').classList.add("d-none");
             document.getElementById('connexion').classList.remove("d-none");
+            break;
+        case 'verifierCompte':
+            document.getElementById('accueilAPI').classList.add("d-none");
+            document.getElementById('creerCompte').classList.add("d-none");
+            document.getElementById('connexion').classList.add("d-none");
+            document.getElementById('verifierCompte').classList.remove("d-none");
             break;
     }
 }
