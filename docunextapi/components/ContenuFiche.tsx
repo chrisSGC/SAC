ea59fa53a393cbb59dd45efcbe2a5ef762dcfb53
Christophe Ferru <christophe.ferru@gmail.com>
2021-08-31 14:14:47 -0400

    fin partie requete de la fiche API

--- docunextapi/components/ContenuFiche.tsx
+++ docunextapi/components/ContenuFiche.tsx
@@ -1,5 +1,13 @@
 import Head from 'next/head';
 
+/*                    <ul className="list-reset flex flex-col">
+                        <li className=" rounded-t relative -mb-px block border p-4 border-grey">Cras justo odio</li>
+                        <li className="relative -mb-px block border p-4 border-grey">Dapibus ac facilisis in</li>
+                        <li className="relative -mb-px block border p-4 border-grey">Morbi leo risus</li>
+                        <li className="relative -mb-px block border p-4 border-grey">Porta ac consectetur ac</li>
+                        <li className="rounded-b relative block border p-4 border-grey">Vestibulum at eros</li>
+                    </ul>*/
+
 const ContenuFiche = ({methodeUtilisee}) => {
 	const badge = methodeUtilisee === "POST" ? "inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-green-100 bg-green-700 rounded": "inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-indigo-100 bg-indigo-700 rounded";
 
@@ -19,6 +27,25 @@ const ContenuFiche = ({methodeUtilisee}) => {
                         <p><span className="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-yellow-100 bg-yellow-500 rounded">Méthode asynchrone</span></p>
                     </div>
                 </div>
+                <div className="my-2"><span className="font-bold">Media type:</span> Application/json</div>
+                <div className="my-2"><span className="font-bold">Type:</span> Object</div>
+                <div className="mt-2"><span className="font-bold">Propriétés:</span></div>
+                <div className="w-1/4 mb-2">
+                    <ul className="list-reset flex flex-col">
+                        <li className=" rounded-t relative -mb-px block border p-4 border-grey"><span className="font-bold">nomCompte:</span> <span className="italic">required (string)</span></li>
+                        <li className="rounded-b relative block border p-4 border-grey"><span className="font-bold">motDePasse:</span> <span className="italic">required (string)</span></li>
+                    </ul>
+                </div>
+                <div className="mt-2"><span className="font-bold">Exemple:</span></div>
+                <div className="bg-gray-300 rounded py-2 px-2 my-2">
+                    <div className="font-mono">
+                        &#123;<br />
+                        nomCompte : 'John Cena',<br />
+                        motDePasse : 'SaintPampamSurMer'<br />
+                        &#125;
+                    </div>
+                </div>
+                <hr className="my-6" />
             </div>
 		</div>
 	)
